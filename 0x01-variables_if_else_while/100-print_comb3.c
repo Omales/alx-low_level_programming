a2cfd6cb981fa91e71d5f57ca65e117bbb85cb1f
sammson omale <samsonomale23@gmail.com>
2022-09-15 07:49:49 -0700

    100-print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- printing numbers from 01-99 with commas
+ * Description: using the main function
+ * this program prints numbers from 01-99
+ * Return: 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 89; d <= 108; d++)
+	{
+		putchar(d);
+		if (d != 108)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
