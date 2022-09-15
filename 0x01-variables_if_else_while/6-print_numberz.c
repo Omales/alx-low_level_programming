d9f37f5fec0998899ab175c31267842efd5754c5
sammson omale <samsonomale23@gmail.com>
2022-09-15 05:50:12 -0700

    possitive integers base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - that prints all single digit numbers of base
+ * Description: using the main function
+ * this program that prints all single digit numbers of base
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+
+	{
+
+		putchar(c);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
