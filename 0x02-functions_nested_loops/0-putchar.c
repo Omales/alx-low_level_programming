b43d8bae35f5391ff925dbba6d95bccf6965888b
sammson omale <samsonomale23@gmail.com>
2022-09-14 17:20:09 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>                                                                                                                                                                
+#include <stdio.h>                   
+#include <time.h>                                                                                                                                                                  
+/**                                                                                                                                                                                 *main - main block                                                                                                                                                               
+ * Get a ramdom number and print the number                                                                                                                                        
+ * and if it positive, negative, or zero                                                                                                                                           
+ */
+
+main (void)
+	int _putchar
+{
+	printf("_putchar,\n");
+	Return (0);
+}
