537ad78b16f44307ccc40e450d7ae18e16f7d0e0
sammson omale <samsonomale23@gmail.com>
2022-09-19 02:36:40 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,17 @@
  * this function prints a-z
  * Return: 0
  */
-void print_alphabet_x10();
+char print_alphabet_x10();
 int main()
 {
-	void print_alphabet_x10();
+	char print_alphabet_x10();
 	return (0);
 }
 /**
  * print_alphabet_x10 - function to print abc 10 times
  * Return: 0
  */
-void print_alphabet_x10(void)
+char print_alphabet_x10()
 {
 	char c, i;
 
