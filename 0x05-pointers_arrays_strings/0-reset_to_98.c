51a97bd7e1f64d7dbb257e029c50c9fe245ecf1d
sammson omale <samsonomale23@gmail.com>
2022-09-19 03:01:48 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <time.h>
+/**
+ * main- this function prints a char
+ * Description: this funtion from main
+ * it prints a character
+ * Return: 0
+ */
+#include "main.h"
+/**
+ * reset_to_98 - Set pointer param to 98.
+ * @n: int type pointer
+ */
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
+
+void reset_to_98 ();
+
+int main()
+
+{
+
+    return (0);
+
+}
