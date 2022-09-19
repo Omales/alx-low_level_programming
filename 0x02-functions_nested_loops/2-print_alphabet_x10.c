f85dbae537f404b5b627a46217aa072affe728a0
sammson omale <samsonomale23@gmail.com>
2022-09-19 01:08:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+	putchar('\n');
+	}
+
+}
+/**
+ * main- prints alpahbets
+ * Description: this function prints alphabets a-z
+ * this function prints from a-z
+ * Return: 0
+ */
+int main(void)
+{
+	void print_alphabet_x10(void);
+	return (0);
+
+}
